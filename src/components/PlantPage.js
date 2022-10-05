91336dcb552696415d3ff240a8ea099fd1829078
stella <sftamesis13@gmail.com>
2022-10-05 12:02:24 -0700

    complete search field

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -6,6 +6,8 @@ import Search from "./Search";
 function PlantPage() {
   //#1: 
   const [plants, setPlants] = useState([])
+  //#4: We are adding searchText state here because both the Search and PlantList will need access to this data.
+  const [searchText, setSearchText] = useState("")
 
   //#1:
   useEffect(() => {
@@ -20,12 +22,20 @@ function PlantPage() {
     setPlants(updatedPlantArray)
   }
 
+  //#4: we want to use the searchText to determine what plant is displayed using the plantList component:
+  const displayedPlants = plants.filter(plant => {
+    return plant.name.toLowerCase().includes(searchText.toLowerCase())
+  })
+
   return (
     <main>
       {/* #2: onAddPlant to pass in the handleAddPlant to newplantform */}
       <NewPlantForm onAddPlant={handleAddPlant}/>
-      <Search />
-      <PlantList plants={plants}/> 
+      <Search 
+        /*pass down the searhText as a prop so that search can access it: */
+        searchText={searchText}
+        onSearchChange={setSearchText}/>
+      <PlantList plants={displayedPlants}/> 
     </main>
   );
 }
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, {useState} from "react";
 
-function Search() {
+function Search({ searchText, onSearchChange }) {
+  
   return (
     <div className="searchbar">
       <label htmlFor="search">Search Plants:</label>
@@ -8,7 +9,10 @@ function Search() {
         type="text"
         id="search"
         placeholder="Type a name to search..."
-        onChange={(e) => console.log("Searching...")}
+        //We want to set the value of whatever is in the search input field
+        value={searchText}
+        //passed setSearchText to reset the value of searchText to what we put in the search field:
+        onChange={(e) => onSearchChange(e.target.value)}
       />
     </div>
   );
